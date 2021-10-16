22997787cdc13a3c07be942287929b0962994f5f
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-16 07:59:18 -0500

    Generando función para aplicación de expresiones regulares y entrega de las sub-strings.

--- Text-Parser.py
+++ Text-Parser.py
@@ -3,13 +3,34 @@
 #import sys <--- This is for debugging purposes
 import re
 
-pattern1 =  "-* show version -*"
-pattern2 = "Configuration register is 0x.*"
-pattern3 = "-* show inventory _*"
-pattern4 = "-* show region -*"
+def filter_text(file_object):
+
+    pattern1 =  "-* show version -*"
+    pattern2 = "Configuration register is 0x.*"
+    pattern3 = "-* show inventory _*"
+    pattern4 = "-* show region -*"
+
+    text = file_object.read()
+    match = re.search(pattern1, text)
+    match2 = re.search(pattern2, text)
+    match3 = re.search(pattern3, text)
+    match4 = re.search(pattern4, text)
+    start1 = match.start()
+    start2 = match2.start()
+    start3 = match3.start()
+    start4 = match4.start()
+    end1 = match.end()
+    end2 = match2.end()
+
+    substring = text[start1:end2]
+    substring2 = text[start3:start4-1]
+
+    return (substring,substring2)
+
+
+
 showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r")
 parsed = open("Parsed Show Tech.txt","w")
-
 
 #Test
 #a = "a"
@@ -59,22 +80,11 @@ parsed = open("Parsed Show Tech.txt","w")
 
 ##-------------------Getting Substring---------------------------------------
 
-text = showtech.read()
-match = re.search(pattern1, text)
-match2 = re.search(pattern2, text)
-match3 = re.search(pattern3, text)
-match4 = re.search(pattern4, text)
-start1 = match.start()
-start2 = match2.start()
-start3 = match3.start()
-start4 = match4.start()
-end1 = match.end()
-end2 = match2.end()
+result = filter_text(showtech)
 
-print("borders identified at",start1,end1,start2,end2)
+snippet, snippet2 = result
 
-snippet = text[start1:end2]
-snippet2 = text[start3:start4-1]
+
 
 """
 Not needed at this moment
