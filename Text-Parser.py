d83b7ca428af553469082d442291da17225efb48
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-25 06:43:16 -0500

    Iniciando pruebas de mejoras de expresiones regulares e incluyendo la parte del show platform como salida del para el archivo de texto filtrado.

--- Text-Parser.py
+++ Text-Parser.py
@@ -2,27 +2,32 @@ import re
 
 def filter_text(file_object):
 
-    pattern1 =  "-* show version -*"
-    pattern2 = "Configuration register is 0x.*"
-    pattern3 = "-* show inventory _*"
-    pattern4 = "-* show region -*"
+    pattern1 =  "--* show version --*"
+    pattern2 = "--* show running-config --*"
+    pattern3 = "-* show inventory --*"
+    pattern4 = "--* show region --*"
+    pattern5 = "--* show platform --*"
+    pattern6 = "--* show romvar --*"
 
     text = file_object.read()
     match = re.search(pattern1, text)
     match2 = re.search(pattern2, text)
     match3 = re.search(pattern3, text)
     match4 = re.search(pattern4, text)
+    match5 = re.search(pattern5, text)
+    match6 = re.search(pattern6, text)
     start1 = match.start()
     start2 = match2.start()
     start3 = match3.start()
     start4 = match4.start()
-    end1 = match.end()
-    end2 = match2.end()
+    start5 = match5.start()
+    start6 = match6.start() 
 
-    substring = text[start1:end2]
+    substring = text[start1:start2-1]
     substring2 = text[start3:start4-1]
+    substring3 = text[start5:start6-1]
 
-    return (substring,substring2)
+    return (substring,substring2,substring3)
 
 
 #Phase 1: Opening file 
@@ -31,12 +36,14 @@ parsed = open("Parsed Show Tech.txt","w")
 
 #Phase 2: Getting substring using regex
 result = filter_text(showtech)
-snippet, snippet2 = result
+snippet, snippet2, snippet3 = result
 
 #Phase 3: Save the substring into text file
 parsed.write(snippet)
 parsed.write("\n\n\n")
 parsed.write(snippet2)
+parsed.write("\n\n\n")
+parsed.write(snippet3)
 
 #Phase 4: Close object files to clean system buffer 
 showtech.close()
