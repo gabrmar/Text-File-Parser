6356d67760670bdec3a0572b2048b2167f742918
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-25 23:57:42 -0500

    ideas iniciales para determinar la ubicación de los patrones para hallar la versión del show version.

--- Number Analyzer.py
+++ Number Analyzer.py
@@ -1,18 +1,11 @@
 import parser
 
 def seekVersion(text):
-    global patterns
     pass
 
 def seekROMMON(text):
-    global patterns
     pass
 
-def SeekPID(text):
-    global patterns
-    pass
-
-patterns = []
 
 #Fase 1: Abrir archivo de interes
 parsed = open("Parsed Show Tech.txt","r")
--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@ import re
 def filter_text(file_object):
 
     patterns = ["--* show version --*","--* show running-config --*","--* show platform --*",
-                   "--* show romvar --*","-* show inventory --*","--* show region --*"]
+                "--* show romvar --*","-* show inventory --*","--* show region --*"]
     text = file_object.read()
     matches = []
     borders = []
@@ -25,3 +25,8 @@ def writer(snippet_list,output_file):
     for i in snippet_list:
         output_file.write(i)
         output_file.write("\n\n")
+
+def something():
+    test = [ "Version [0-9][0-9].[0-9][0-9].[0-9][0-9]",
+    "R0        [0-9]*            [0-9][0-9].[0-9]\([0-9]{1,2}r\)"]
+    pass
