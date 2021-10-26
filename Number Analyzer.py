6a39f0dc9d88a5c12d5a8c7d93e9e6a6b0e6b4c6
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-25 21:17:30 -0500

    Inicnando bosquejo para las funciones que buscarán números dentro del archivo filtrado.

--- Number Analyzer.py
+++ Number Analyzer.py
@@ -1,3 +1,13 @@
 
+def seekVersion(text):
+    pass
+
+def seekROMMON(text):
+    pass
+
+def SeekPID(text):
+    pass
+
 #Fase 1: Abrir archivo de interes
 parsed = open("Parsed Show Tech.txt","r")
+#Fase 2: Selección de los valores numéricos que son de interes
