f0db0e8f5ed504e5c037af0ae6651d800e0e8be3
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-25 21:21:07 -0500

    iniciando bosquejo de lista de patrones regex para el código.

--- Number Analyzer.py
+++ Number Analyzer.py
@@ -8,6 +8,8 @@ def seekROMMON(text):
 def SeekPID(text):
     pass
 
+patterns = []
+
 #Fase 1: Abrir archivo de interes
 parsed = open("Parsed Show Tech.txt","r")
 #Fase 2: Selección de los valores numéricos que son de interes
