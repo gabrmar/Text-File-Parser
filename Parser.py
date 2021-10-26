009cbbdbac60871f396dc3ee61e5424878174c7c
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-25 22:39:44 -0500

    Trasladando de funciones e importación de módulo de expresiones regulares a su propio archivo.

---
+++ Parser.py
@@ -0,0 +1,27 @@
+import re
+
+def filter_text(file_object):
+
+    patterns = ["--* show version --*","--* show running-config --*","--* show platform --*",
+                   "--* show romvar --*","-* show inventory --*","--* show region --*"]
+    text = file_object.read()
+    matches = []
+    borders = []
+    substrings = []
+    k = 0
+
+    for i in patterns:
+        matches.append(re.search(i,text))
+    for j in matches:
+        borders.append(j.start())
+
+    while k <= len(borders)/2 + 1: #colocar notas al respecto de como funciona este ciclo
+        substrings.append(text[borders[k]:borders[k+1]-1])
+        k=k+2
+        
+    return substrings
+
+def writer(snippet_list,output_file):
+    for i in snippet_list:
+        output_file.write(i)
+        output_file.write("\n\n")
--- Text-Parser.py
+++ Text-Parser.py
@@ -1,41 +1,15 @@
-import re
-
-def filter_text(file_object):
-
-    patterns = ["--* show version --*","--* show running-config --*","--* show platform --*",
-                   "--* show romvar --*","-* show inventory --*","--* show region --*"]
-    text = file_object.read()
-    matches = []
-    borders = []
-    substrings = []
-    k = 0
-
-    for i in patterns:
-        matches.append(re.search(i,text))
-    for j in matches:
-        borders.append(j.start())
-
-    while k <= len(borders)/2 + 1: #colocar notas al respecto de como funciona este ciclo
-        substrings.append(text[borders[k]:borders[k+1]-1])
-        k=k+2
-        
-    return substrings
-
-def writer(snippet_list,output_file):
-    for i in snippet_list:
-        output_file.write(i)
-        output_file.write("\n\n")
+import Parser
 
 #Phase 1: Opening file 
 showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r")
 parsed = open("Parsed Show Tech.txt","w")
 
 #Phase 2: Getting substring using regex
-result = filter_text(showtech)
+result = Parser.filter_text(showtech)
 #listed = list(result)
 
 #Phase 3: Save the substring into text file
-writer(result,parsed)
+Parser.writer(result,parsed)
 
 #Phase 4: Close object files to clean system buffer 
 showtech.close()
