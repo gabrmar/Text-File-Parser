e5090b08b61494ed06932e44bd161e4e866c65cd
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-10-26 22:39:29 -0500

    Corrgiendo el nombre de las funciones para encajar con e módulo tparser

--- Text-Parser.py
+++ Text-Parser.py
@@ -1,15 +1,15 @@
-import parser
+import tparser
 
 #Phase 1: Opening file 
 showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r")
 parsed = open("Parsed Show Tech.txt","w")
 
 #Phase 2: Getting substring using regex
-result = parser.filter_text(showtech)
+result = tparser.filter_text(showtech)
 #listed = list(result)
 
 #Phase 3: Save the substring into text file
-parser.writer(result,parsed)
+tparser.writer(result,parsed)
 
 #Phase 4: Close object files to clean system buffer 
 showtech.close()
