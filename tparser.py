811ca3af5bb4a1ad3c7b85a24d43b2e5cf34ff4b
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-11-06 23:06:34 -0500

    Agregando comentarios básicos y organizando funciones del módulo tparser.

--- tparser.py
+++ tparser.py
@@ -1,6 +1,49 @@
 import re
 
-def ROM2numbers(text):
+
+
+def filter_text(file_object): #Función de filtrado del show tech 
+
+    """Esta es la función principal de filtrado. Tiene los patrones fundamentales para filtrar un 
+    archivo de show tech"""
+
+    patterns = ["--* show version --*","--* show running-config --*","--* show platform --*",
+                "--* show romvar --*","-* show inventory --*","--* show region --*"]
+    text = file_object.read()
+    matches = []
+    borders = []
+    substrings = []
+    k = 0
+
+    for i in patterns:
+        matches.append(re.search(i,text))
+    for j in matches:
+        borders.append(j.start())
+
+    while k <= len(borders)/2 + 1: #Obtiene porciones del show tech de acuerdo a las fronteras obtenidas
+        #Por medio de expresiones regulares
+        substrings.append(text[borders[k]:borders[k+1]-1])
+        k=k+2
+        
+    return substrings
+
+def writer(snippet_list,output_file): #Función de escritura de secciones filtradas
+    
+    for i in snippet_list:
+        output_file.write(i)
+        output_file.write("\n\n")
+
+def versionChecker(version): #Pendiente de definir
+    pass
+
+#---------------------ROMMON Routines------------------------------------------------------#
+
+def ROM2numbers(text): #Esta función extrae los números que componen la versión de ROMMON 
+
+    """El objetivo de esta función es partir el pedazo de la cadena de caracteres que tiene le versión
+    de ROMMON en diferntes sub-cadenas para cada uno de las cifras de la versión (normalmente 3 cifras).
+    Cada cifra es almacenada en una lista serpada"""
+
     num = text.split(".")
     num2 = []
     num3 = []
@@ -21,52 +64,24 @@ def ROM2numbers(text):
 
     return numbers
 
-            
+def ROMMON_Validator(ROMMON): #Función central para el manejo de la versión de ROMMON
 
-def filter_text(file_object):
+    """Esta rutina es el cuerpo principal donde se encontrarán las sub-rutinas relacionados con 
+    el análisis de la versión de ROMMON"""
 
-    patterns = ["--* show version --*","--* show running-config --*","--* show platform --*",
-                "--* show romvar --*","-* show inventory --*","--* show region --*"]
-    text = file_object.read()
-    matches = []
-    borders = []
-    substrings = []
-    k = 0
-
-    for i in patterns:
-        matches.append(re.search(i,text))
-    for j in matches:
-        borders.append(j.start())
-
-    while k <= len(borders)/2 + 1: #colocar notas al respecto de como funciona este ciclo
-        substrings.append(text[borders[k]:borders[k+1]-1])
-        k=k+2
-        
-    return substrings
-
-def writer(snippet_list,output_file):
-    
-    for i in snippet_list:
-        output_file.write(i)
-        output_file.write("\n\n")
-
-def versionChecker(version):
-    pass
-
-def ROMMON_Validator(ROMMON):
     suggestion = open("Suggested ROMMON.txt","r")
     file = suggestion.read()
     version = re.search("[0-9]{1,2}.[0-9]{1,2}\([0-9]{1,2}r\)",file)
     version_text = version.group()
-    suggested_numbers = ROM2numbers(version_text)
-    ROMMON_numbers = ROM2numbers(ROMMON)
+    suggested_numbers = ROM2numbers(version_text) #Obtener los números que componen el ROMMON
+    ROMMON_numbers = ROM2numbers(ROMMON) #Lo mismo aquí
 
     return (suggested_numbers,ROMMON_numbers,version_text)
 
-def compareROMMON(suggestedROM,currentROM):
+def compareROMMON(suggestedROM,currentROM): #Función de comparación  de versiones de ROMMON
     pass
 
-def searchROMMON(text):
+def searchROMMON(text): #Función de extracción de la versión de ROMMON 
     patterns = ["R0        [0-9]*            [0-9]{1,2}.[0-9]\([0-9]{1,2}r\)"]
     matches = []
 
@@ -74,7 +89,8 @@ def searchROMMON(text):
         matches.append(re.search(i,text))
     
     firmware = matches[0].group()
-    match2 = re.search("[0-9]{1,2}.[0-9]\([0-9]{1,2}r\)",firmware)
+    match2 = re.search("[0-9]{1,2}.[0-9]\([0-9]{1,2}r\)",firmware) #Removiendo de la línea lo que no tiene
+    #que ver con la versión de ROMMON
     number = match2.group()
     suggested, ROMMON, notes = ROMMON_Validator(number)
     print(suggested)
@@ -82,7 +98,10 @@ def searchROMMON(text):
 
     return (number,notes)
 
-def searchVersion(text):
+#---------------------IOS/IOS-XE Routines------------------------------------------------------#
+
+
+def searchVersion(text): #Función de extracción de la versión de IOS O IOS-XE
 
     patterns = [ "Version [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"]
     matches = []
